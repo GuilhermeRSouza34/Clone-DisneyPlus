9f11455b9ca40c315374a64d2bee8df24eaab8e7
Guilherme <guilhermersouza34@gmail.com>
2024-03-15 18:43:54 -0300

    Adicionando Js ao projeto

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    
+}) 

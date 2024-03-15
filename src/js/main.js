d7491ad07954ebd1b8b625cb89ccb8987a05e21e
Guilherme <guilhermersouza34@gmail.com>
2024-03-15 18:46:58 -0300

    Ajustes no scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
-}) 
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
